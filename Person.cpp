7dd9d104115e26ea4a5d43f120d99eec36c40ef1
Abdalla Mustafa <Abdallahpostatmix@gmail.com>
2026-02-06 05:04:19 +0200

    Refactor project: split headers and cpp for Person and Validation

--- Person.cpp
+++ Person.cpp
@@ -1,1 +1,34 @@
 #include "Person.h"
+
+
+Person::Person(const string& name, const string& password, int id) :name("no_name"), password("no_password"), id(id)
+{
+	set_name(name);
+	set_password(password);
+}
+
+bool Person::set_name(const string& name) {
+	if (Validation::is_valid_name(name)) {
+		this->name = name;
+		return true;
+	}
+	else {
+		cout << "Invalid name! Try again.\n";
+		return false;
+	}
+}
+
+bool Person::set_password(const string& password) {
+	if (Validation::is_valid_password(password)) {
+		this->password = password;
+		return true;
+	}
+	else {
+		cout << "Invalid password! Try again.\n";
+		return false;
+	}
+}
+
+const string& Person::get_name() const { return name; }
+const string& Person::get_password() const { return password; }
+int Person::get_id() const { return id; }
--- Person.h
+++ Person.h
@@ -7,33 +7,11 @@ protected:
 	string name, password;
 	int id;
 public:
-	Person(const string& name,const string& password,int id) :name("no_name"),password("no_password") ,id(id)
-	{
-		set_name(name);
-		set_password(password);		
-	}
-	bool set_name(const string& name) {
-		if (Validation::is_valid_name(name)) {
-			this->name = name;
-			return true;
-		}
-		else {
-			cout << "Invalid name! Try again.\n";
-			return false;
-		}
-	}
-	bool set_password(const string& password) {
-		if (Validation::is_valid_password(password)) {
-			this->password = password;
-			return true;
-		}
-		else {
-			cout << "Invalid password! Try again.\n";
-			return false;
-		}
-	}
-	const string& get_name() const { return name; }
-	const string& get_password() const { return password; }
-	int get_id() const { return id; }
+	Person(const string& name, const string& password, int id);
+	bool set_name(const string& name);
+	bool set_password(const string& password);
+	const string& get_name()const;
+	const string& get_password()const;
+	int get_id()const;
 };
 
