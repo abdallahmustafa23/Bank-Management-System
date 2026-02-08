ce49156ef8c1b737d15d220cee16e831f57384b6
am14689045-rgb <am.14689045@gmail.com>
2026-02-08 21:16:47 +0200

    Implement Employee class with salary management

---
+++ Empolyee.cpp
@@ -0,0 +1,31 @@
+#include "Employee.h"
+#include <iostream>
+
+using namespace std;
+
+Employee::Employee(const string& name, const string& password, int id, double salary)
+    : Person(name, password, id), salary(0)
+{
+    set_salary(salary);
+}
+
+bool Employee::set_salary(double salary)
+{
+    if (Validation::is_valid_salary(salary)) {
+        this->salary = salary;
+        return true;
+    }
+    cout << "Invalid salary! Must be >= 5000\n";
+    return false;
+}
+
+double Employee::get_salary() const {
+    return salary;
+}
+
+void Employee::display() const
+{
+    cout << "Employee ID: " << id << endl;
+    cout << "Name: " << name << endl;
+    cout << "Salary: " << salary << endl;
+}
